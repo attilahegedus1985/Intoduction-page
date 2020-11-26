d846dee3976f8053c9ac18a0c77d1672fd4eda6a
Attila Hegedus <attila.hegedus.1985@gmail.com>
2020-11-26 15:28:10 +0100

    first version with jQuery
    first version with jQuery

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".hege").mouseenter(function() {
+      $(".hege").animate({opacity: "0.1"});
+    $(".hege").mouseleave(function() {
+      $(".hege").animate({opacity: "1"});
+        
+                      });
+  });
+  
+  });
+  
